19cf316905999b6aca4a0749304beb88a97b12e2
defloored <p.b@berkeley.edu>
2022-02-20 22:02:59 -0800

    Implemented night time classification for splits

--- create_splits.py
+++ create_splits.py
@@ -4,9 +4,10 @@ import os
 import random
 
 import numpy as np
+import cv2
 
 from utils import get_module_logger
-from random import shuffle
+from utils import get_dataset
 import shutil
 
 
@@ -22,19 +23,59 @@ def split(data_dir):
     # TODO: Split the data present in `/home/workspace/data/waymo/training_and_validation` into train and val sets.
     # You should move the files rather than copy because of space limitations in the workspace.
 
+    # We are given 3 tfrecords for testing in the VM workspace and we will keep it as-is. The other files
+    # will be split 70/30 into training and evaluation respectively.
+
+    # Before we do the split, we should evenly distribute the night time records.
+    # To do this, we find all night time tfrecords by using OpenCV to compute average brightness, and filter by threshold.
+
     data = glob.glob(data_dir + '/training_and_validation/*.tfrecord')
     os.makedirs(os.path.abspath(data_dir + "/train"), exist_ok=True)
     os.makedirs(os.path.abspath(data_dir + "/val"), exist_ok=True)
+    daytime = []
+    nighttime = []
+    mypath = "data/waymo/training_and_validation/"
 
-    # Split to 90% train, 10% val
-    cutoff = len(data)//10 + 1
-    shuffle(data)
-    for data_file in data[:cutoff]:
+    for filename in data:
+        daynightdataset = get_dataset(filename)
+        daynightdataset = daynightdataset.take(1)
+        for batch in daynightdataset:
+            if (is_night(batch)):
+                nighttime.append(filename)
+            else:
+                daytime.append(filename)
+
+    random.shuffle(daytime)
+    random.shuffle(nighttime)
+    # Split to 70% train, 30% val
+    dcutoff = (len(daytime)*3) // 10 + 1
+    ncutoff = (len(nighttime)*3) // 10 + 1
+
+    for data_file in daytime[:dcutoff]:
         shutil.move(data_file, data_dir + '/val/' + os.path.basename(data_file))
-    for data_file in data[cutoff:]:
+    for data_file in daytime[dcutoff:]:
+        shutil.move(data_file, data_dir + '/train/' + os.path.basename(data_file))
+    for data_file in nighttime[:ncutoff]:
+        shutil.move(data_file, data_dir + '/val/' + os.path.basename(data_file))
+    for data_file in nighttime[ncutoff:]:
         shutil.move(data_file, data_dir + '/train/' + os.path.basename(data_file))
 
+def is_night (batch):
+    """
+    Determine if tf record is night time.
 
+    args:
+        - batch [numpy arr]: sample image from tfrecord
+    """
+    img = batch['image'].numpy()
+    # convert image to HSV
+    hsv = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+    # compute mean of brightness
+    brightnessavg = np.sum(hsv[:,:,2]) / (hsv.shape[0] * hsv.shape[1])
+    # display only if night time, using a brightness threshold
+    if (brightnessavg < 70):
+        return True
+    return False
 
 
 if __name__ == "__main__": 
